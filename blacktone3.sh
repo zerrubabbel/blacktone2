c5d78877008ec6fb9689276cb360e43f5843f3d5
Kurone <zerrubabbel@gmail.com>
2026-06-21 18:46:25 -0400

    Configure Blacktone software with necessary files
    Added configuration steps for Blacktone software, including copying configuration files for .bashrc, fastfetch, kitty, and icons.

--- blacktone3.sh
+++ blacktone3.sh
@@ -130,19 +130,67 @@ sleep 2
 
 # SYSTEM CONFIGURATION
 
+echo "Configuring Blacktone software... "
+
 ## .bashrc and arch-notes
+
+cp "$SCRIPT_DIR/configs/.bashrc" "$HOME/.bashrc"
+cp "$SCRIPT_DIR/configs/.arch-notes" "$HOME/.arch-notes"
 
 ## fastfetch
 
+if [[ ! -d "$HOME/.config/fastfetch/" ]]; then
+    mkdir -p "$HOME/.config/fastfetch/"
+fi
+
+cp "$SCRIPT_DIR/configs/config.jsonc" "$HOME/.config/fastfetch/config.jsonc"
+cp "$SCRIPT_DIR/configs/logo.png" "$HOME/.config/fastfetch/logo.png"
+
 ## kitty
+
+if [[ ! -d "$HOME/.config/kitty/" ]]; then
+    mkdir -p "$HOME/.config/kitty/"
+fi
+
+cp "$SCRIPT_DIR/configs/kitty.conf" "$HOME/.config/kitty/kitty.conf"
 
 ## Wallpeppers and icons
 
+if [[ ! -d "$HOME/Pictures/" ]]; then
+    mkdir -p "$HOME/Pictures/"
+fi
+
+cp -r "$SCRIPT_DIR/configs/Wallpeppers" "$HOME/Pictures/Wallpeppers/"
+
+if [[ ! -d "/usr/share/icons/" ]]; then
+    sudo mkdir -p "/usr/share/icons/"
+fi
+
+sudo tar -xvf "$SCRIPT_DIR/configs/Bibata-Original-Classic.tar.xz" -C "/usr/share/icons/"
+sudo tar -xzvf "$SCRIPT_DIR/configs/BeautyDream-GTK-20240521180535.tar.gz" -C "/usr/share/icons/"
+
+if [[ ! -f "/etc/environment" ]]; then
+    sudo touch "/etc/environment"
+fi
+
+sudo cp "/etc/environment" "/etc/environemtn.bak"
+cp "$SCRIPT_DIR/configs/environment" "/etc/environment"
+
 # CINNAMON CONFIGURATION
+
+
 
 # COSMIC CONFIGURATION
 
+
+
 # CLEANUP AND EXIT
+
+clear
+echo "Blacktone software is configured... "
+sleep 4
+exit 0
+
 
 
 
